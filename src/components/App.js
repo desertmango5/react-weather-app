8ccc85ee1d0e240a4ad84e988fe60dfa407a28c4
Michael Manges <michael.manges@gmail.com>
2018-03-12 12:12:15 +0400

    Temperature unit convert button

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,19 @@ import '../styles/default-styles.css';
 import '../styles/App.css';
 import { apiKey } from '../private';
 import WeatherCard from './WeatherCard';
+import ConvertTemp from './ConvertTemp';
 import weatherData from '../data/weatherData';
 
 class App extends Component {
   state = {
     weather: {},
+    isLoaded: false,
+    error: null,
+    forecasts: {},
+    tempUnit: 'F',
   };
 
+  // connect to Open Weather API
   componentDidMount() {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=London&mode=json&APPID=${apiKey}`)
       .then(res => res.json())
@@ -17,29 +23,59 @@ class App extends Component {
         (result) => {
           this.setState({
             weather: result,
+            forecasts: result.list,
+            isLoaded: true,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
           });
         }
       )
   };
 
-  updateWeather = (data) => {
-    this.setState({ weather: data });
+  convertTempF = (temp) => {
+    const f = (((temp - 273.15) * 1.8) + 32);
+    return Math.round(f);
   };
+  
+  convertTempC = (temp) => {
+    const c = (temp - 273.15);
+    return Math.round(c);
+  };
+
+  farhenheitUnits = () => {
+    const unit = this.state.tempUnit === 'F' ? 'F' : 'F';
+    this.setState({ tempUnit: unit });
+  }
+  
+  celciusUnits = () => {
+    const unit = this.state.tempUnit === 'C' ? 'C' : 'C';
+    this.setState({ tempUnit: unit });
+  }
 
   render() {
     return (
       <div className="App">
         <h1 className="title">5-Day Forecast</h1>
         <section className="weather-cards">
-          {Object.keys(this.state.weather).map(key => (
+          {Object.keys(this.state.forecasts).map(key => (
               <WeatherCard 
                 key={key}
-                id={key}
-                data={this.state.weather[key]}
+                forecasts={this.state.forecasts[key]}
                 className="weather-card"
+                convertTempF={this.convertTempF}
+                convertTempC={this.convertTempC}
+                tempUnit={this.state.tempUnit}
               />
           ))}
         </section>
+        <ConvertTemp 
+          farhenheitUnits={this.farhenheitUnits}
+          celciusUnits={this.celciusUnits}
+        />
       </div>
     );
   }
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -4,16 +4,21 @@ import icon from '../assets/weather-icon.png';
 
 class WeatherCard extends React.Component {
   render() {
-    console.log(this.props.data[0]);
-    // const high = this.props.data[0].main.temp_max;
+    const high = this.props.forecasts.main.temp_max;
+    const low = this.props.forecasts.main.temp_min;
+    const { convertTempF, convertTempC, tempUnit } = this.props;
 
     return (
       <div className="weather-card">
         <p className="weather-card__day" />
         <img src={icon} alt="weather icon" className="weather-card__icon" />
         <section className="weather-card__temp">
-          <p className="weather-card__temp-high">&deg;</p>
-          <p className="weather-card__temp-low">&deg;</p>
+          <p className="weather-card__temp-high">
+            {tempUnit === 'F' ? `${convertTempF(high)}` : `${convertTempC(high)}`}&deg;
+          </p>
+          <p className="weather-card__temp-low">
+            {tempUnit === 'F' ? `${convertTempF(low)}` : `${convertTempC(low)}`}&deg;
+          </p>
         </section>
       </div>
     );
